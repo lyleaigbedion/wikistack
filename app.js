cab4ac30a964970ad13f2a81530a426b28efc08c
lyleaigbedion <Lyle-Anthony.Aigbedion75@myhunter.cuny.edu>
2020-04-30 17:15:36 -0400

    Sequelized Models described

---
+++ app.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const morgan = require('morgan');
+const { db } = require('./models');//STILL REQIURED TO RUN
+const models =  require('./models');// gives us access to any and all values exported from './models' folder
+
+const layout = require("./views/layout");
+
+db.authenticate().
+then(() =>{
+  console.log("connected to the database");
+});
+
+const init = async () => {
+  try{
+    await models.db.sync({force: true});
+    console.log("The database for the models was (re)created!")
+  } catch(error){ console.error(error)}
+
+};
+
+init();
+
+
+const app =  express(); //runs express
+
+app.use(morgan("dev"));
+app.use(express.urlencoded({extended: false}));
+app.use(express.static(__dirname + "/public/style"));//path where static files exist
+
+app.get("/",(req,res)=>{
+  res.send(layout(""));
+});
+
+const PORT = 3000;
+
+app.listen(PORT,()=>{
+  console.log(`App listening on ${PORT}`);
+});
+
+
